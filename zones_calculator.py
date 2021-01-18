49dae0e0eae36d4637a04afa8a74c68b0ccad338
Joe <57957983+joseph-cavarretta@users.noreply.github.com>
2021-01-18 16:14:39 -0700

    Update zones_calculator.py

--- zones_calculator.py
+++ zones_calculator.py
@@ -1,77 +1,90 @@
 ##  FLATIRONS ENDURANCE TRAIANING ZONES CALCULATOR ##
 
 ## HEART RATE TRAINING ZONES CALCULATOR ##
+## Enter LT Heart Rate ##
 LThr = 170
 
-Z1hr = (LThr-20)
-Z2hr = (LThr-10)
-Z3hr = (LThr)
-Z4hr = (LThr+10)
-Z5hr = (LThr+20)
+## Heart Rate Zones Defined ##
+Z1hr = (LThr-40)
+Z2hr = (LThr-20)
+Z3hr = (LThr-8)
+Z4hr = (LThr)
+Z5hr = (LThr+8)
+Z6hr = (LThr+20)
 
-print(" ")
-print("--- HEART RATE TRAINING ZONES ---")
-print(" ")
-print("Threshold HR = " + str(LThr))
-print(" ")
-print(f'Zone 1: Endurance        = < {Z1hr} bpm')
-print(f'Zone 2: Tempo            = {Z1hr} bpm - {Z2hr} bpm')
-print(f'Zone 3: Threshold        = {Z2hr} bpm - {Z3hr} bpm')
-print(f'Zone 4: Interval         = {Z3hr} bpm - {Z4hr} bpm')
-print(f'Zone 5: Speed            = {Z4hr} bpm - {Z5hr} bpm')
+## Print Heart Rate Zones ##
+print("\n--- HEART RATE TRAINING ZONES ---\n")
+print("Threshold HEART RATE = " + str(LThr)+"\n")
+print(f'Zone 1: Recovery        = < {Z1hr}')
+print(f'Zone 2: Endurance       = {Z1hr} - {Z2hr}')
+print(f'Zone 3: Tempo           = {Z2hr} - {Z3hr}')
+print(f'Zone 4: Sub-Threshold   = {Z3hr} - {Z4hr}')
+print(f'Zone 5: Supra-Threshold = {Z4hr} - {Z5hr}')
+print(f'Zone 6: VO2             = > {Z5hr}')
 
 ## RUNNING POWER TRAINING ZONES CALCULATOR ##
+## Enter LT Power ##
 LTpwr = 300
 
-Z1pwr = int(LTpwr*0.8)
-Z2pwr = int(LTpwr*0.9)
-Z3pwr = int(LTpwr)
-Z4pwr = int(LTpwr*1.1)
-Z5pwr = int(LTpwr*1.20)
+## Power Zones Defined ##
+Z1pwr = int(LTpwr*0.6)
+Z2pwr = int(LTpwr*0.8)
+Z3pwr = int(LTpwr*0.92)
+Z4pwr = int(LTpwr)
+Z5pwr = int(LTpwr*1.08)
+Z6pwr = int(LTpwr*1.15)
 
-print(" ")
-print("--- POWER TRAINING ZONES ---")
-print(" ")
-print("Threshold POWER = " + str(LTpwr))
-print(" ")
-print(f'Zone 1: Endurance        = < {Z1pwr}W')
-print(f'Zone 2: Tempo            = {Z1pwr}W - {Z2pwr}W')
-print(f'Zone 3: Threshold        = {Z2pwr}W - {Z3pwr}W')
-print(f'Zone 4: Interval         = {Z3pwr}W - {Z4pwr}W')
-print(f'Zone 5: Speed            = {Z4pwr}W - {Z5pwr}W')
-    
+## Print Power Training Zones ##
+print("\n--- POWER TRAINING ZONES ---\n")
+print("Threshold POWER = " + str(LTpwr)+"\n")
+print(f'Zone 1: Recovery        = < {Z1pwr}')
+print(f'Zone 2: Endurance       = {Z1pwr} - {Z2pwr}')
+print(f'Zone 3: Tempo           = {Z1pwr} - {Z2pwr}')
+print(f'Zone 4: Sub-Threshold   = {Z1pwr} - {Z2pwr}')
+print(f'Zone 5: Supra-Threshold = {Z1pwr} - {Z2pwr}')
+print(f'Zone 6: VO2             = {Z1pwr} - {Z2pwr}')
+print(f'Zone 7: Neuro/AC           = > {Z1pwr}')
+
 ## RUNNING PACE ZONES CALCULATOR ##
+## Enter LT Pace in Min/Mile ##
 LTpace = '7:00'
+
+## Convert Min/Mile to Seconds/Mile ##
 split_pace = LTpace.split(":")
 minutes = int(split_pace[0])
 seconds = int(split_pace[1])
 pace = minutes * 60 + seconds
 
-Z1pace = int(pace*1.2)
-Z2pace = int(pace*1.1)
-Z3pace = int(pace)
-Z4pace = int(pace*0.90)
-Z5pace = int(pace*0.80)
+## Pace Zones Defined ##
+Z1pace = int(pace*1.4)
+Z2pace = int(pace*1.2)
+Z3pace = int(pace*1.08)
+Z4pace = int(pace)
+Z5pace = int(pace*0.92)
+Z6pace = int(pace*0.85)
 
+## Convert Seconds/Mile to Minutes/Mile ##
 Z1min = Z1pace//60
 Z2min = Z2pace//60
 Z3min = Z3pace//60
 Z4min = Z4pace//60
 Z5min = Z5pace//60
+Z6min = Z6pace//60
 
 Z1sec = Z1pace % 60
 Z2sec = Z2pace % 60
 Z3sec = Z3pace % 60
 Z4sec = Z4pace % 60
 Z5sec = Z5pace % 60
+Z6sec = Z6pace % 60
 
-print(" ")
-print("--- PACE TRAINING ZONES ---")
-print(" ")
-print("Threshold PACE = " + str(LTpace))
-print(" ")
-print(f'Zone 1: Endurance        = < {Z1min}:{Z1sec:02d}/mi')
-print(f'Zone 2: Tempo            = {Z1min}:{Z1sec:02d}/mi - {Z2min}:{Z2sec:02d}/mi')
-print(f'Zone 3: Threshold        = {Z2min}:{Z2sec:02d}/mi - {Z3min}:{Z3sec:02d}/mi')
-print(f'Zone 4: Interval         = {Z3min}:{Z3sec:02d}/mi - {Z4min}:{Z4sec:02d}/mi')
-print(f'Zone 5: Speed            = {Z4min}:{Z4sec:02d}/mi - {Z5min}:{Z5sec:02d}/mi')
+## Print Pace Training Zones ## 
+print("\n--- PACE TRAINING ZONES ---\n")
+print("Threshold PACE = " + str(LTpace)+"\n")
+print(f'Zone 1: Recovery        = < {Z1min}:{Z1sec:02d}')
+print(f'Zone 2: Endurance       = {Z1min}:{Z1sec:02d} - {Z2min}:{Z2sec:02d}')
+print(f'Zone 3: Tempo           = {Z2min}:{Z2sec:02d} - {Z3min}:{Z3sec:02d}')
+print(f'Zone 4: Sub-Threshold   = {Z3min}:{Z3sec:02d} - {Z4min}:{Z4sec:02d}')
+print(f'Zone 5: Supra-Threshold = {Z4min}:{Z4sec:02d} - {Z5min}:{Z5sec:02d}')
+print(f'Zone 6: VO2             = {Z5min}:{Z5sec:02d} - {Z6min}:{Z6sec:02d}')
+print(f'Zone 7: Neuro/AC       = > {Z6min}:{Z6sec:02d}')
